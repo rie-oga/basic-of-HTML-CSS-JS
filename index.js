546d5ad2d5c78148cb87f1e9584329dbc515218a
rie <kou.kotetsu@gmail.com>
2020-11-20 18:38:46 +0900

    ok

---
+++ index.js
@@ -0,0 +1,14 @@
+document.getElementById('timer').innerHTML = '00:01:00';//→表示は00:01:00になる
+//document.getElementByIdメソッドは、パラメータにHTMLの要素のid属性を指定することで、HTML内の要素を取得するためのもの
+//.innerHTMLはメソッドではなくプロパティー。プロパティーは見た目を表す。内容を書き換えるという意味。
+
+/*
+要素の取得①
+getElementById idで取得
+要素の取得②
+getElementsByClassName class名で取得　「配列」の形で取得される
+要素の取得③
+getElementsByName name属性を使用　フォームの要素を取得するときなどに利用
+要素の取得④
+getElementsByTagName タグ名を使って要素を取得
+*/
